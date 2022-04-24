f022b0be2d02543d31986bf3b2c1fe8f866d5176
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-24 23:04:11 +0100

    add SubjectDao

---
+++ src/main/java/com/omarox/school/dao/SubjectDao.java
@@ -0,0 +1,24 @@
+package com.omarox.school.dao;
+
+import com.omarox.school.model.Subject;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SubjectDao {
+    int insertSubject(UUID id, Subject subject);
+
+    default int insertSubject(Subject subject) {
+        UUID id = UUID.randomUUID();
+        return insertSubject(id, subject);
+    }
+
+    List<Subject> selectAllSubjects();
+
+    Optional<Subject> selectSubjectById(UUID id);
+
+    int deleteSubjectById(UUID id);
+
+    int updateSubjectById(UUID id);
+}
