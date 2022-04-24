b33fcea0f64a5c7d319bb5ab9e82736f789ba4a7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-24 23:06:07 +0100

    add TeacherDao

---
+++ src/main/java/com/omarox/school/dao/TeacherDao.java
@@ -0,0 +1,24 @@
+package com.omarox.school.dao;
+
+import com.omarox.school.model.Teacher;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TeacherDao {
+    int insertTeacher(UUID id, Teacher teacher);
+
+    default int insertTeacher(Teacher teacher) {
+        UUID id = UUID.randomUUID();
+        return  insertTeacher(id, teacher);
+    }
+
+    List<Teacher> selectAllTeachers();
+
+    Optional<Teacher> selectTeacherById(UUID id);
+
+    int deleteTeacherById(UUID id);
+
+    int updateTeacherById(UUID id);
+}
