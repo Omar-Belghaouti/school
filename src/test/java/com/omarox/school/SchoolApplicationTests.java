8300ce3ec3068cdf1d6112dcbd7305afbce42c98
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-24 22:25:58 +0100

    setup project

---
+++ src/test/java/com/omarox/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarox.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
