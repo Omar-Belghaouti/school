a37965efdc2771a517e31ed430baf0b3c356d576
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-24 22:59:38 +0100

    add ClassroomDao

---
+++ src/main/java/com/omarox/school/dao/ClassroomDao.java
@@ -0,0 +1,24 @@
+package com.omarox.school.dao;
+
+import com.omarox.school.model.Classroom;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ClassroomDao {
+    int insertClassroom(UUID id, Classroom classroom);
+
+    default int insertClassroom(Classroom classroom) {
+        UUID id = UUID.randomUUID();
+        return insertClassroom(id, classroom);
+    }
+
+    List<Classroom> selectAllClassrooms();
+
+    Optional<Classroom> selectClassroomById(UUID id);
+
+    int deleteClassroomById(UUID id);
+
+    int updateClassroomById(UUID id);
+}
